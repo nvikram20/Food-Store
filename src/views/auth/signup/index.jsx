316eb80d3dca2901871972d0f2305f3d1069367a
Niharika Vikram <niharikavikram327@gmail.com>
2025-04-01 00:00:43 -0700

    fixed signin and signup text issue.

--- src/views/auth/signup/index.jsx
+++ src/views/auth/signup/index.jsx
@@ -32,7 +32,7 @@ const SignUp = ({ history }) => {
   const dispatch = useDispatch();
 
   useScrollTop();
-  useDocumentTitle('Sign Up | Salinaka');
+  useDocumentTitle('Sign Up | Samriddhi');
 
   useEffect(() => () => {
     dispatch(setAuthStatus(null));
