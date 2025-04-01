aa6f02605bc7ea94476724720bf65ad605e7867c
Niharika Vikram <niharikavikram327@gmail.com>
2025-03-31 23:34:26 -0700

    Initial commit with modifications

---
+++ src/views/home/index.jsx
@@ -0,0 +1,92 @@
+import { ArrowRightOutlined } from '@ant-design/icons';
+import { MessageDisplay } from '@/components/common';
+import { ProductShowcaseGrid } from '@/components/product';
+import { FEATURED_PRODUCTS, RECOMMENDED_PRODUCTS, SHOP } from '@/constants/routes';
+import {
+  useDocumentTitle, useFeaturedProducts, useRecommendedProducts, useScrollTop
+} from '@/hooks';
+import bannerImg from '@/images/banner-girl.png';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Home = () => {
+  useDocumentTitle('Salinaka | Home');
+  useScrollTop();
+
+  const {
+    featuredProducts,
+    fetchFeaturedProducts,
+    isLoading: isLoadingFeatured,
+    error: errorFeatured
+  } = useFeaturedProducts(6);
+  const {
+    recommendedProducts,
+    fetchRecommendedProducts,
+    isLoading: isLoadingRecommended,
+    error: errorRecommended
+  } = useRecommendedProducts(6);
+
+  return (
+    <main className="content">
+      <div className="home">
+        <div className="banner">
+          <div className="banner-desc">
+            <h1 className="text-thin">
+              <strong>Taste</strong>
+              &nbsp;the Tradition of&nbsp;
+              <strong>South India</strong>
+            </h1>
+            <p>
+            There’s a burst of flavor in every jar of our handcrafted pickles—made with fresh ingredients and age-old recipes passed down through generations. Experience the spicy, tangy, and utterly authentic taste of home, delivered right to your doorstep.
+            </p>
+            <br />
+            <Link to={SHOP} className="button">
+              Shop Now &nbsp;
+              <ArrowRightOutlined />
+            </Link>
+          </div>
+          <div className="banner-img"><img src={bannerImg} alt="" /></div>
+        </div>
+        <div className="display">
+          <div className="display-header">
+            <h1>Featured Products</h1>
+            <Link to={FEATURED_PRODUCTS}>See All</Link>
+          </div>
+          {(errorFeatured && !isLoadingFeatured) ? (
+            <MessageDisplay
+              message={errorFeatured}
+              action={fetchFeaturedProducts}
+              buttonLabel="Try Again"
+            />
+          ) : (
+            <ProductShowcaseGrid
+              products={featuredProducts}
+              skeletonCount={6}
+            />
+          )}
+        </div>
+        <div className="display">
+          <div className="display-header">
+            <h1>Recommended Products</h1>
+            <Link to={RECOMMENDED_PRODUCTS}>See All</Link>
+          </div>
+          {(errorRecommended && !isLoadingRecommended) ? (
+            <MessageDisplay
+              message={errorRecommended}
+              action={fetchRecommendedProducts}
+              buttonLabel="Try Again"
+            />
+          ) : (
+            <ProductShowcaseGrid
+              products={recommendedProducts}
+              skeletonCount={6}
+            />
+          )}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
